4132dce4271b217ef41aeacfeef5722b434d2508
nnapolitx <nnapolitano88@gmail.com>
2023-03-13 20:41:45 -0300

    finished 3.4

---
+++ curso_1.R
@@ -0,0 +1,10 @@
+# NXN 13/03/2022
+# Estadistica descriptiva
+mean(c(2, 3))
+mean(2:10) 
+sd(c(2,3))
+sd(2:3)
+metallica <- c("Lars", "James", "Jason", "Kirk")
+metallica2 <- metallica[metallica!='Jason']
+soda <- c('cerati', 'zeta', 'charly')
+soda[1]
