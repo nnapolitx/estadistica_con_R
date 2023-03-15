cfe858080d7dca4dbc7fb84da056757baef8d037
nnapolitx <nnapolitano88@gmail.com>
2023-03-15 20:39:51 -0300

    starting section4 / class 2

---
+++ 4-graficos.R
@@ -0,0 +1,14 @@
+# Class 02- Chapter 4 on the website
+# notes on class
+getwd()
+setwd("/home/nick/utalca/estadistica_con_R/data")
+# common libraries for working with graphics and plots
+library(ggplot2)
+library(reshape)
+library(plyr)
+library(Rmisc)
+# important ideas when drawing graphics:
+# Generally, try to use as little "ink" as possible
+# KISS method
+# Try not to distortion the data
+# using large, coherent data sets
